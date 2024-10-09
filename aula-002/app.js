d31275be84f89ccf64b29c40c237f5e73513c9d3
Alberto Kitumba <alberkitumba@gmail.com>
2024-10-09 18:39:44 +0100

    Adiona os arquivos da aula 002

---
+++ aula-002/app.js
@@ -0,0 +1,37 @@
+"use strict";
+// var foraDoIf = "Mensagem fora do if";
+// if (true) {
+//   var dentroDoIf = "Mensagem dentro do if";
+// }
+// function executaExemplo() {
+//   var dentroDaFunc = "Mensagem dentro da função";
+// }
+// console.log(dentroDaFunc)
+// console.log(foraDoIf);
+// console.log(dentroDoIf);
+// let foraDoIf = "Mensagem fora do if";
+// if (true) {
+//   let dentroDoIf = "Mensagem dentro do if";
+// }
+// console.log(foraDoIf);
+// console.log(dentroDoIf);
+// const nomeDoMeuPaisDeNascenca = "Angola";
+// nomeDoMeuPaisDeNascenca = "Argelia"
+let nome = "Cipriano";
+const sobrenome = "Sunguahanga";
+var idade = 19;
+// const mensagem = nome + " " + sobrenome + " tem " + idade + " anos de idade.";
+const mensagem = `${nome} ${sobrenome} tem ${idade} anos de idade`;
+const texto = `
+  Nikola Tesla (/ˈtɛslə/;[2] Serbian Cyrillic: Никола Тесла, [nǐkola têsla]; 10 July 1856[a] – 7 January 1943) was a Serbian-American[3][4] engineer, futurist, and inventor. He is known for his contributions to the design of the modern alternating current (AC) electricity supply system.[5]
+
+Born and raised in the Austrian Empire, Tesla first studied engineering and physics in the 1870s without receiving a degree. He then gained practical experience in the early 1880s working in telephony and at Continental Edison in the new electric power industry. In 1884 he immigrated to the United States, where he became a naturalized citizen. He worked for a short time at the Edison Machine Works in New York City before he struck out on his own. With the help of partners to finance and market his ideas, Tesla set up laboratories and companies in New York to develop a range of electrical and mechanical devices. His AC induction motor and related polyphase AC patents, licensed by Westinghouse Electric in 1888, earned him a considerable amount of money and became the cornerstone of the polyphase system which that company eventually marketed.
+
+Attempting to develop inventions he could patent and market, Tesla conducted a range of experiments with mechanical oscillators/generators, electrical discharge tubes, and early X-ray imaging. He also built a wirelessly controlled boat, one of the first ever exhibited. Tesla became well known as an inventor and demonstrated his achievements to celebrities and wealthy patrons at his lab, and was noted for his showmanship at public lectures. Throughout the 1890s, Tesla pursued his ideas for wireless lighting and worldwide wireless electric power distribution in his high-voltage, high-frequency power experiments in New York and Colorado Springs. In 1893, he made pronouncements on the possibility of wireless communication with his devices. Tesla tried to put these ideas to practical use in his unfinished Wardenclyffe Tower project, an intercontinental wireless communication and power transmitter, but ran out of funding before he could complete it.
+
+`;
+console.log(nome.length);
+console.log(nome.indexOf("g"));
+console.log(texto.length);
+console.log(texto.indexOf("Tesla"));
+console.log(nome);
