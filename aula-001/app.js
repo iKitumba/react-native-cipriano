d51fa5edbf3cb0a8290245a769adf0ea2dd1ba76
Alberto Kitumba <alberkitumba@gmail.com>
2024-10-09 17:59:23 +0100

    Coloca a pasta das dependencias fora da pasta aula 001

---
+++ aula-001/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// let nome = "Alberto";
+// const idade: number = 20;
+// let casado: boolean = false;
+const objeto = {
+    nome: "Alberto",
+    idade: 10,
+};
+console.table(objeto);
+// https://github.com/ikitumba/react-native-cipriano
